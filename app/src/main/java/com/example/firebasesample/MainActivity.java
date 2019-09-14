36f5b38ed07437747df1b724f667008e98a587a1
Gurjas Singh <gurjassingh92@gmail.com>
2019-09-14 18:21:19 +0530

    Firebase app updated for Student Management

--- app/src/main/java/com/example/firebasesample/MainActivity.java
+++ app/src/main/java/com/example/firebasesample/MainActivity.java
@@ -1,48 +1,65 @@
 package com.example.firebasesample;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getName();
+    EditText studentName, studentAddress, studentPhone;
+    Button saveRecord;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(getApplicationContext());
 
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Student");
+        studentName = findViewById(R.id.enter_name);
+        studentAddress = findViewById(R.id.enter_address);
+        studentPhone = findViewById(R.id.enter_phone);
+        saveRecord = findViewById(R.id.save_record);
 
-        Log.d(TAG, "database values " + databaseReference);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Student");
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        saveRecord.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                Log.d(TAG, "Data snapshot value: " + dataSnapshot);
-                long values = dataSnapshot.getChildrenCount();
-                for (int i = 0; i<values; i++) {
-                    DataSnapshot data = dataSnapshot.child(String.valueOf(i));
-                    Log.d(TAG, "data value: " + data);
-                    String name = (String) data.child("name").getValue();
-                    long phone = (long) data.child("phone").getValue();
-                    String address = (String) data.child("address").getValue();
-                    Log.d(TAG, "Student details are: " + name + phone + address);
-                }
-            }
+            public void onClick(View v) {
+                String name = studentName.getText().toString();
+                String address = studentAddress.getText().toString();
+                long phone = Long.parseLong(studentPhone.getText().toString());
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e(TAG, databaseError.getMessage());
+                // create hash map
+                HashMap<String, Object> student = new HashMap<>();
+                student.put("name", name);
+                student.put("address", address);
+                student.put("phone", phone);
+
+                // save values to database
+                databaseReference.push().setValue(student);
+
+                Intent intent = new Intent();
+                setResult(101, intent);
+                finish();
             }
         });
+
+
     }
 }
